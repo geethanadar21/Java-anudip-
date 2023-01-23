a64b1fac0f3bb829a2416b5a5428fcde756a1a5e
geethanadar21 <116073272+geethanadar21@users.noreply.github.com>
2023-01-23 12:34:57 +0530

    Add files via upload

---
+++ Practical/Hello.java
@@ -0,0 +1,11 @@
+class My{
+  public static void display(){
+    System.out.println("Helloo everyone...");
+  }
+}
+class Hello{
+  public static void main (String args[]){
+    My.display();
+    System.out.println("This is my first program");
+  }
+}
